adee77040bdde632f5288566b97646ffc82ff267
alibama <adp6j@virginia.edu>
2021-04-13 14:06:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ url="https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=malaria&forma
 df = pd.read_json(url)
 #df = pd.DataFrame(df['result'].values.tolist())
 print (df.head())
+
+st.write(df)
 print (url)
 """
 # Why no data
