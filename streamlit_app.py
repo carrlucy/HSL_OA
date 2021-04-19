17dae45887e94f2cb4f3e02302b220ea98582d11
alibama <adp6j@virginia.edu>
2021-04-19 14:54:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ for x in xmldoc.iter('resultList'):
     st.write("hello")
     for result in root1.iter('result'):
         root2=ET.Element('xmldoc')
-        st.write(result.isOpenAccess)
+        st.write(result)
 
 
 
