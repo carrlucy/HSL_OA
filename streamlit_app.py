13b347bf32126795098d2a48d50da830dc231f20
carrlucy <61550284+carrlucy@users.noreply.github.com>
2021-04-19 16:53:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import json
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as et
 import urllib.request
 import rdflib
 from urllib.request import urlopen
@@ -33,13 +34,43 @@ xmldoc = parse(var_url)
 #st.write(xml_element)
 
 #https://towardsdatascience.com/converting-multi-layered-xml-files-to-dataframes-in-python-using-xmltree-a13f9b043b48
-for x in xmldoc.iter('resultList'):
-    root1=ET.Element('xmldoc')
-    root1=x
-    st.write("hello")
-    for x2 in root1.iter('result'):
+#for x in xmldoc.iter('resultList'):
+    #root1=ET.Element('xmldoc')
+   #root1=x
+    #st.write("hello")
+    #for x2 in root1.iter('result'):
        # root2=ET.Element('xmldoc')
-        st.write(x2.pmid)
+        #st.write(x2.pmid)
+
+tree=et.parse('https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=University%20of%20Virginia&resultType=core&format=xml)
+    # need to include result type= core - above only gets us lite metadata not including affiliaiton- https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=%22University%20of%20Virginia%22&resultType=core&cursorMark=*&pageSize=25&format=xml
+
+
+DOI=[]
+AuthorFullName=[]
+AuthorAffiliation=[]
+
+for x in root.iter('result'):
+    root1=et.Element('root')
+    root1=x
+    for writer in root1.iter('author'):
+        root2 = et.Element('root')
+        st.write(writer)
+        root2=(writer)
+        for authorInstitution in root2.iter('authorAffiliation'):
+            root3 = et.Element('root')
+            st.write(authorInstitution)
+            root3=authorInstitution
+            for authorInstitution in root4.iter('authorAffiliation'):
+                DOI.append(result.attrib['doi'])
+                AuthorFullName.append(writer.attrib['fullName'])
+                AuthorAffiliation.append(authorInstitution.attrib['affiliation'])
+df = pd.DataFrame({'DOI': 
+DOI,'AuthorFullName':AuthorFullName,'AuthorAffiliation':AuthorAffiliation })
+                        
+                
+
+         
 
 
 #for x in root.iter('region'):
