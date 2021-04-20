1439bdf8db32021a76152de9b40850913a093f13
alibama <adp6j@virginia.edu>
2021-04-20 17:34:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ for a in root[4]:
     root1=a
     for b in root1.iter('isOpenAccess'):
         root2=ET.Element('root')
-        st.write(b.tag)
+        st.write(b.tag, "contains", b.text)
     for c in root1.iter('authorString'):
         st.write(c.text)
     for d in root1.iter('firstPublicationDate'):
