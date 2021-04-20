702a219db2aded29e519707b78b3f0a7ca4af5a4
alibama <adp6j@virginia.edu>
2021-04-20 13:01:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,7 @@ from xml.etree.ElementTree import parse
         #st.write(x2.)
 
 #tree=ET.parse('https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=University%20of%20Virginia&resultType=core&format=xml')
-tree=ET.parse('https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=university%20of%20virginia&resultType=core&cursorMark=*&pageSize=100&format=xml
-')
+tree=ET.parse('https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=university%20of%20virginia&resultType=core&cursorMark=*&pageSize=35&format=xml')
 root = tree.getroot()
 st.write(root)
     
