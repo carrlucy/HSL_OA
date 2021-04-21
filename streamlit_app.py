05facd0591be83359e1a03fb50830982d0af57ab
alibama <adp6j@virginia.edu>
2021-04-20 22:20:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ from xml.etree.ElementTree import parse
 """
 searchThis=st.sidebar.text_input('Query')
 test1="Virginia"
-buildQuery="https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=test&resultType=core&cursorMark=*&pageSize=35&format=xml
+buildQuery="https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=test&resultType=core&cursorMark=*&pageSize=35&format=xml"
 
 #https://www.foxinfotech.in/2019/04/python-how-to-read-xml-from-url.html
 restQuery=urlopen(buildQuery)
