99b48ae79571f094d8fe89b46f3ae0647c53718a
alibama <adp6j@virginia.edu>
2021-04-22 15:58:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,8 @@ df['year']=df['date'].dt.to_period('Y')
 df['yearDate'] = df['year'].astype(str)
 df3 = df[['yearDate', 'openAccess'],['doi']].copy()
 
-dfChart=df3.groupby(['yearDate'],['openAccess'])['doi'].value_count
+dfChart=df3.groupby(['yearDate'],['openAccess'])['doi'].count.reset_index()
+
 
 st.write(dfChart)
 #st.write(dfChart.describe())
