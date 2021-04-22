5e486de6be230bb99f3357767104740598537b53
carrlucy <61550284+carrlucy@users.noreply.github.com>
2021-04-22 07:42:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ for a in root[4]:
 
     
 
-df = pd.DataFrame({'authors':authors,'date':date,'title':title,'iso':iso,'doi':doi, 'openAccess': openAccess,'})
+df = pd.DataFrame({'authors':authors,'date':date,'title':title,'iso':iso,'doi':doi,'openAccess': openAccess,})
 df['date'] = pd.to_datetime(df['date'])
 
 #df = pd.DataFrame({'openAccess':openAccess,'authors':authors,'date':date,'title':title,'iso':iso,'doi':doi,'})
