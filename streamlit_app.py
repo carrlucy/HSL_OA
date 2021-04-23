3df7067eefe1113e3cd8b4acdbfbb878f57008f7
carrlucy <61550284+carrlucy@users.noreply.github.com>
2021-04-23 11:26:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ df['date'] = pd.to_datetime(df['date'])
 
 
 openFilter = sorted(df['openAccess'].drop_duplicates()) # select the open access values 
-#open_Filter = st.sidebar.selectbox('Open Access?', openFilter) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
+open_Filter = st.sidebar.selectbox('Open Access?', openFilter) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
 df2=df[df['openAccess'].str.contains(open_Filter)] # create a dataframe filtered below
 st.write(df2.sort_values(by='date'))
 
