c490c21aad9ef6961f7840a9b0fd3b37bd8b16db
alibama <adp6j@virginia.edu>
2021-04-28 12:28:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,24 +20,32 @@ for col in ['oa','author','year','title','iso','doi','id','cited']:
 cr_mrk= '' #current cursor mark
 nxt_mrk = '*' #next cursor mark
 
-while cr_mrk != nxt_mrk:              
-    url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?'
-    query = '(AFF:"University of Virginia") AND (FIRST_PDATE:[2020-12-01 TO 2020-12-31]) AND (HAS_FT:Y)'
-    params = {'query':query, 'resultType':'core', 'synonym':'TRUE','cursorMark':nxt_mrk,'pageSize':'1000','format':'json'}
-    response = requests.get(url,params)
-    rjson = response.json()
-    #print(rjson)
-    cr_mrk = urlparse.unquote(rjson['request']['cursorMark'])
-    nxt_mrk = urlparse.unquote(rjson['nextCursorMark'])
-    for rslt in rjson['resultList']['result']:
-        dct['author'].append(rslt['authorString']) if 'authorString' in rslt.keys() else dct['author'].append(0)
-        dct['year'].append(rslt['pubYear']) if 'pubYear' in rslt.keys() else dct['year'].append(0)
-        dct['title'].append(rslt['title']) if 'title' in rslt.keys() else dct['title'].append(0)
-        dct['iso'].append(rslt['isoabbreviation']) if 'isoabbreviation' in rslt.keys() else dct['iso'].append(0)
-        dct['doi'].append(rslt['doi']) if 'doi' in rslt.keys() else dct['doi'].append(0)
-        dct['id'].append(rslt['id']) if 'id' in rslt.keys() else dct['id'].append(0)
-        dct['oa'].append(rslt['isOpenAccess']) if 'isOpenAccess' in rslt.keys() else dct['oa'].append(0)
-        dct['cited'].append(rslt['citedByCount']) if 'citedByCount' in rslt.keys() else dct['cited'].append(0)        
+fulltext_list
+
+menu = ["Y", "N"]
+st.sidebar.subheader("Select Option")
+choice = st.sidebar.selectbox("Full Text", menu)
+
+@st.cache(persist= True)
+def bigask (choice)
+    while cr_mrk != nxt_mrk:              
+        url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?'
+        query = '(AFF:"University of Virginia") AND (FIRST_PDATE:[2020-12-01 TO 2020-12-31]) AND (HAS_FT:'choice')'
+        params = {'query':query, 'resultType':'core', 'synonym':'TRUE','cursorMark':nxt_mrk,'pageSize':'1000','format':'json'}
+        response = requests.get(url,params)
+        rjson = response.json()
+        #print(rjson)
+        cr_mrk = urlparse.unquote(rjson['request']['cursorMark'])
+        nxt_mrk = urlparse.unquote(rjson['nextCursorMark'])
+        for rslt in rjson['resultList']['result']:
+            dct['author'].append(rslt['authorString']) if 'authorString' in rslt.keys() else dct['author'].append(0)
+            dct['year'].append(rslt['pubYear']) if 'pubYear' in rslt.keys() else dct['year'].append(0)
+            dct['title'].append(rslt['title']) if 'title' in rslt.keys() else dct['title'].append(0)
+            dct['iso'].append(rslt['isoabbreviation']) if 'isoabbreviation' in rslt.keys() else dct['iso'].append(0)
+            dct['doi'].append(rslt['doi']) if 'doi' in rslt.keys() else dct['doi'].append(0)
+            dct['id'].append(rslt['id']) if 'id' in rslt.keys() else dct['id'].append(0)
+            dct['oa'].append(rslt['isOpenAccess']) if 'isOpenAccess' in rslt.keys() else dct['oa'].append(0)
+            dct['cited'].append(rslt['citedByCount']) if 'citedByCount' in rslt.keys() else dct['cited'].append(0)        
 
         #print(dct)
 #@st.cache
