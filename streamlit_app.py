8fb5aa263a23f64acf75e41cbb229ba41de6a0c8
alibama <adp6j@virginia.edu>
2021-05-06 19:20:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def bigask ():
             dct['id'].append(rslt['id']) if 'id' in rslt.keys() else dct['id'].append(0)
             dct['oa'].append(rslt['isOpenAccess']) if 'isOpenAccess' in rslt.keys() else dct['oa'].append(0)
             dct['cited'].append(rslt['citedByCount']) if 'citedByCount' in rslt.keys() else dct['cited'].append(0)  
-            dct['journal'].append(rslt['journalInfo']['journal']['title']) if ['journalInfo']['journal']['title'] in rslt.keys() else dct['journal'].append(0)
+            dct['journal'].append(rslt['journalInfo']) if ['journalInfo'] in rslt.keys() else dct['journal'].append(0)
     df=pd.DataFrame.from_dict(dct, orient='columns')
         #print(dct)
     return df
