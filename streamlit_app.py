68f473217b2b704436150c0443c649fc2d255074
carrlucy <61550284+carrlucy@users.noreply.github.com>
2021-05-07 11:36:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,8 +92,8 @@ st.altair_chart(valLayer, use_container_width=True)
 
 
 
-valLayer = alt.Chart(dfdata).mark_line().encode(x='year',y='count(oa)', 'row_count',color='oa')#
-
+valLayer = alt.Chart(dfdata).mark_line().encode(x='year',y='count(oa)', color='oa')#
+valLayer = alt.Chart(dfdata).mark_line().encode(x='year',y='row_count', color='oa')#
 st.altair_chart(valLayer, use_container_width=True)
 
 
